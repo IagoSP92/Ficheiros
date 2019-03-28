a32fa653642e9a47f32fc91f2724cf693bafcc2b
Alumno <Alumno@PC-INFOR00>
2019-03-28 13:51:54 +0100

    a

---
+++ src/Intrucciones.java
@@ -0,0 +1,28 @@
+
+public class Intrucciones {
+//	
+//	fuente de datos semiestricturada
+//	binding/mapao como minimo 2 clases
+	
+//	1:
+
+//	
+//	List<Ob negocioa> agrir (string f)
+//	
+//	guardar (list <obxetonegocio> data, string  f)
+//	
+//	opcional: add(fii f, obxetonegocio o)
+//
+//	
+//	
+//	2: //non gardan en disco, tood e en memoeria -> non tgen file por ningun lado
+//		
+//		obxetonegocio get (string  pseudopk)
+//		
+//		list obxetonegocio/mapa    getBy ( par1, par2 ,... )
+//		
+//		add (obxetonegocio o)
+//		
+//		
+//		3_test
+}
